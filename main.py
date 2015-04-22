21ca02ff2e2de237f1d81b08fc091e7be79a5b42
Dane Pilcher <dane.pilcher@icloud.com>
2015-04-22 11:04:02 -0400

    remove raw input

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ from moviepy.editor import *
 
 def main():
     '''main function'''
-    file_path = raw_input("Filepath:")
-    '''
-    start_point_raw = raw_input("Start point:")
-    end_point_raw = raw_input("End point:")
-    start_point = raw_time_to_sec(start_point_raw)
-    end_point = raw_time_to_sec(end_point_raw)
-    '''
-    start_point_min = int(raw_input("Start point minute:"))
-    start_point_second = int(raw_input("Start point second:"))
-    end_point_min = int(raw_input("End point minute:"))
-    end_point_second = int(raw_input("End point second:"))
     clip = (VideoFileClip(file_path)
             .subclip((start_point_min,start_point_second),(end_point_min,end_point_second)))
     clip.write_gif("newgif1.gif")
