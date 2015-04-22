429e013586f7f29cd0f774c30090319632494484
Dane Pilcher <dane.pilcher@icloud.com>
2015-04-22 11:11:46 -0400

    switch to cli input

--- main.py
+++ main.py
@@ -13,9 +13,16 @@ from moviepy.editor import *
 
 def main():
     '''main function'''
+    file_path = sys.argv[1]
+    raw_start_time = int(sys.argv[2])
+    raw_end_time = int(sys.argv[3])
+    start_point_min = 0
+    start_point_second = raw_start_time
+    end_point_min = 0
+    end_point_second = raw_end_time
     clip = (VideoFileClip(file_path)
             .subclip((start_point_min,start_point_second),(end_point_min,end_point_second)))
-    clip.write_gif("newgif1.gif")
+    clip.write_gif("newgif.gif")
 
 def raw_time_to_sec(raw_time):
     '''takes in a string raw_time in the format 00:00 and returns an
