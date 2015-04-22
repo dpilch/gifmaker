5fcf957b3aade8800fb1f66571ece3419aa36252
Dane Pilcher <dane.pilcher@icloud.com>
2015-04-21 22:14:24 -0400

    added moviepy

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ main.py
 Dane Pilcher
 '''
 
+from moviepy.editor import *
+
 def main():
     '''main function'''
     print "gifmaker"
@@ -13,7 +15,6 @@ def main():
     end_point_raw = raw_input("End point:")
     start_point = raw_time_to_sec(start_point_raw)
     end_point = raw_time_to_sec(end_point_raw)
-    
     
 def raw_time_to_sec(raw_time):
     '''takes in a string raw_time in the format 00:00 and returns an
