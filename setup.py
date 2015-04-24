101aaabaaa40054b0b0fa70cc0d1065e343076b8
Dane Pilcher <dane.pilcher@icloud.com>
2015-04-24 16:08:49 -0400

    create setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='gifmaker',
+    version=0.0.1,
+    url='https://github.com/dpilch/gifmaker',
+    license='LICENCE',
+    author='Dane Pilcher',
+    author_email='dane.pilcher@icloud.com',
+    description='Make a GIF',
+    packages=['gifmaker'],
+    classifiers=[]
+)
